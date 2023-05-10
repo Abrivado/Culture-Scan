993aaf1b9b5dce5b76cd1c5cb93909a441c3a46e
Clement <clement@MacBook-Air-de-Clement.local>
2023-05-10 20:21:55 +0200

    page camera ok

--- Routes.js
+++ Routes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react'; // On importe useState pour gérer l'état des icones
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
@@ -10,43 +10,53 @@ import Map from './pages/Map';
 const Tab = createBottomTabNavigator();
 
 const Routes = () => {
+  const [homeColor, setHomeColor] = useState('#6E798C'); // On crée un état pour la couleur de l'icone home
+  const [mapColor, setMapColor] = useState('#6E798C'); // On crée un état pour la couleur de l'icone map
+
   return (
     <NavigationContainer>
       <Tab.Navigator
-         screenOptions={{
-            tabBarShowLabel: false,
-            tabBarStyle: [{
-              display: 'flex'
-            }, null],
-          }}
+        screenOptions={{
+          tabBarShowLabel: false,
+          tabBarStyle: [{
+            display: 'flex'
+          }, null],
+        }}
       >
         <Tab.Screen
           name="Home"
           component={Home}
           options={{
             tabBarIcon: ({ color, size }) => (
-              <Ionicons name="home" color={color} size={size} />
+              // On utilise la variable homeColor pour déterminer la couleur de l'icone home
+              <Ionicons name="home" color={homeColor} size={size} />
             ),
+          }}
+          // On ajoute un listener pour changer la couleur de l'icone home quand on accède à sa page
+          listeners={{
+            focus: () => setHomeColor('#53A1E8'),
+            blur: () => setHomeColor('#6E798C'),
           }}
         />
         <Tab.Screen
-          name="Camera"
+          name="Scannez un QR code"
           component={Camera}
           options={{
             tabBarIcon: ({ color, size }) => (
+              // On garde le même code pour l'icone camera
               <View
                 style={{
                   position: 'relative',
-                  backgroundColor: '#fff',
+                  backgroundColor: '#53A1E8',
                   borderRadius: 50,
-                  width: 80,
-                  height: 80,
+                  width: 75,
+                  height: 75,
                   justifyContent: 'center',
                   alignItems: 'center',
-                  top: -25,
+                  top: -15,
                 }}
               >
-                <Ionicons name="camera" color={color} size={size} />
+                <Ionicons name="camera" color="white" size={32} />
               </View>
             ),
           }}
@@ -56,8 +66,14 @@ const Routes = () => {
           component={Map}
           options={{
             tabBarIcon: ({ color, size }) => (
-              <Ionicons name="map" color={color} size={size} />
+              // On utilise la variable mapColor pour déterminer la couleur de l'icone map
+              <Ionicons name="map" color={mapColor} size={size} />
             ),
+          }}
+          // On ajoute un listener pour changer la couleur de l'icone map quand on accède à sa page
+          listeners={{
+            focus: () => setMapColor('#53A1E8'),  // couleur bleue quand l'icone est sélectionnée
+            blur: () => setMapColor('#6E798C'),   // couleur grise quand l'icone n'est pas sélectionnée
           }}
         />
       </Tab.Navigator>
--- pages/Camera.js
+++ pages/Camera.js
@@ -1,12 +1,82 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
+import { Ionicons } from '@expo/vector-icons';
 
-const Camera = () => {
-  return (
-    <View>
-      <Text>Welcome to the Camera page!</Text>
-    </View>
-  );
+const CameraPage = () => {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+  const [flashMode, setFlashMode] = useState(Camera.Constants.FlashMode.off);
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleCameraType = () => {
+    setType(
+      type === Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back
+    );
+  };
+
+  const handleFlashMode = () => {
+    setFlashMode(
+      flashMode === Camera.Constants.FlashMode.off
+        ? Camera.Constants.FlashMode.torch
+        : Camera.Constants.FlashMode.off
+    );
+  };
+
+  if (hasCameraPermission === null) {
+    return <View />;
+  } else if (hasCameraPermission === false) {
+    return <Text>No access to camera</Text>;
+  } else {
+    return (
+      <View style={styles.container}>
+        <Camera style={styles.camera} type={type} flashMode={flashMode} ref={cameraRef}>
+          <View style={styles.cameraView}>
+            <TouchableOpacity style={styles.cameraIcon} onPress={handleFlashMode}>
+              <Ionicons name={flashMode === Camera.Constants.FlashMode.off ? 'flash-off' : 'flash'} size={30} color="#fff" />
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.cameraIcon} onPress={handleCameraType}>
+              <Ionicons name="camera-reverse" size={30} color="#fff" />
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      </View>
+    );
+  }
 };
 
-export default Camera;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#000',
+  },
+  camera: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  cameraView: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 10,
+  },
+  cameraIcon: {
+    margin: 10,
+  },
+});
+
+export default CameraPage;
